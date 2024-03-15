42303d7eb05e0e978f76755f18ce4f7169b197e7
tuncerdamat <tuncer.damat@gmail.com>
2024-03-15 20:23:56 +0100

    Inject the PostsRepository service and make it available for usage

--- wp-content/plugins/dependency-injection/ClassInit.php
+++ wp-content/plugins/dependency-injection/ClassInit.php
@@ -2,5 +2,17 @@
 
 class ClassInit
 {
+    /**
+     * @param PostsRepository $postsRepository
+     */
+    public function __construct(private readonly PostsRepository $postsRepository)
+    {
+    }
 
+    public function run() {
+
+        $post = $this->postsRepository->getSomePostByName();
+        
+        // Do some stuff with the post, update etc. 
+    }
 }
